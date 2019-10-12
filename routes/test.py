5d7041130d1e4a04f0ad0865ff4d7c5a32d94db5
Justin Schmitt <jschmitt@live.unc.edu>
2019-10-12 14:53:32 -0400

    guess

--- routes/test.py
+++ routes/test.py
@@ -2,6 +2,6 @@ import sys
 import pandas as pd
 import lightgbm as lgb
 
-#model = lgb.Booster(model_file='fraud-model.txt')
+model = lgb.Booster(model_file='fraud-model.txt')
 
 print("hello world " + sys.argv[1])
