ae537e8ca503fcb451318ca60f019fdbb5df040b
Justin Schmitt <jschmitt@live.unc.edu>
2019-10-12 14:35:16 -0400

    update python to 3.7

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,6 @@ var express = require('express');
 var router = express.Router();
 const ps = require('python-shell');
 
-let options = {
-  mode: 'text',
-  pythonPath: '/usr/bin/python3',
-  pythonOptions: ['-u'], // get print results in real-time
-  scriptPath: '/opt/bitnami/apps/hacknc2019/routes',
-  args: ['value1', 'value2']
-};
 
 
 /* GET home page. */
@@ -17,6 +10,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/predict', function(req, res) {
+  let options = {
+    mode: 'text',
+    pythonPath: '/usr/bin/python3.7',
+    pythonOptions: ['-u'], // get print results in real-time
+    scriptPath: '/opt/bitnami/apps/hacknc2019/routes',
+    args: ['value1', 'value2']
+  };
   ps.PythonShell.run('test.py', options, function (err, results) {
     if (err) throw err;
     console.log('finished');
