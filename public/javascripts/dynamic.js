8f5d78a759238988b9cf40d441bb488cd169d08c
Justin Schmitt <jschmitt@live.unc.edu>
2019-10-12 23:05:16 -0400

    frontend js7

--- public/javascripts/dynamic.js
+++ public/javascripts/dynamic.js
@@ -3,7 +3,39 @@ $(document).ready(function(){
     $('#submitButton').click(function(){
         console.log(document.getElementById('card1').value);
 
-        
+        var message = {
+            "card1":254346,
+            "card2":60.0,
+            "card3":170.0,
+            "card5":102.0,
+            "TransactionAmt":3524.0,
+            "TransactionDT":89901,
+            "addr1":325.0,
+            "dist1":4000,
+            "dist2":150,
+            "D4":0.0,
+            "D2":100,
+            "D10":0.0,
+            "D8":0,
+            "D1":0.0,
+            "D9":0,
+            "C13":1.0,
+            "C1":1.0,
+            "C2":1.0,
+            "C4":0.0,
+            "C5":0.0,
+            "C6":1.0,
+            "C7":0.0,
+            "C8":0.0,
+            "C9":0.0,
+            "C10":0.0,
+            "C11":1.0,
+            "C12":0.0
+        }
+
+        $.post("index.js", message, function(result){
+            console.log(result);
+          });
 
     });
 
