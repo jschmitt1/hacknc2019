ec0ee89f1d3193626d867aa6a0299ec89cb41bf1
Justin Schmitt <jschmitt@live.unc.edu>
2019-10-13 11:08:18 -0400

    auto-fraud12

--- public/javascripts/dynamic.js
+++ public/javascripts/dynamic.js
@@ -206,13 +206,13 @@ $(document).ready(function(){
             document.getElementById('prugressBur').value = prediction;
             if (prediction < 25) {
                 document.getElementById('prugressBur').className = "progress is-success is-large";
-                document.getElementsByName("someName").value = "No Fraud Detected"
+                document.getElementsByName("someName")[0].innerText = "No Fraud Detected"
             } else if (prediction > 70) {
                 document.getElementById('prugressBur').className = "progress is-danger is-large";
-                document.getElementsByName("someName").value = "Fraud Detected"
+                document.getElementsByName("someName")[0].innerText = "Fraud Detected"
             } else {
                 document.getElementById('prugressBur').className = "progress is-warning is-large";
-                document.getElementsByName("someName").value = "Potential Fraud"
+                document.getElementsByName("someName")[0].innerText = "Potential Fraud"
             }
             document.getElementById('detectionSubtitle').innerText = `Based on the model, the transaction has an estimated ${prediction}% chance of being fraudulent.`;
             
